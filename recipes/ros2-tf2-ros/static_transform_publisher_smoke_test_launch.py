ea1e3fe69115bf8d0bc134920bea1546b5a345aa
GitHub Action <action@github.com>
2026-09-15 15:20:37 +0000

    Add build files 2026-09-15-1520

---
+++ recipes/ros2-tf2-ros/static_transform_publisher_smoke_test_launch.py
@@ -0,0 +1,47 @@
+import sys
+
+from launch import LaunchDescription
+import launch_ros.actions
+import launch_pytest
+import pytest
+
+
+@pytest.fixture
+def static_transform_publisher():
+    return launch_ros.actions.Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        output="screen",
+        cached_output=True,
+        arguments=[
+            "--frame-id", "world",
+            "--child-frame-id", "child",
+        ],
+        additional_env={
+            "RMW_IMPLEMENTATION": "rmw_zenoh_cpp",
+            "RCUTILS_LOGGING_USE_STDOUT": "1",
+            "ZENOH_ROUTER_CHECK_ATTEMPTS": "-1",
+        },
+    )
+
+@launch_pytest.fixture
+def launch_description(static_transform_publisher):
+    return LaunchDescription([
+        static_transform_publisher,
+    ])
+
+@pytest.mark.skipif(
+    sys.platform == "darwin",
+    reason="Post-shutdown exit codes are unreliable on macOS.",
+)
+@pytest.mark.launch(fixture=launch_description)
+def test_node_initializes(static_transform_publisher, launch_context):
+    def validate_output(output):
+        assert "Spinning until stopped - publishing transform" in output
+
+    launch_pytest.tools.process.assert_output_sync(
+        launch_context,
+        static_transform_publisher,
+        validate_output,
+        timeout=10,
+    )
